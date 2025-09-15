95e0a3d098634e9ebf244e3f74eb028b010e7b8b
bartangutan <19bartosz11@gmail.com>
2025-09-15 14:31:12 +0200

    typy proste

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        //typy proste, zmienne (nie obniekty) - małe litery
+
+        //typy całkowite
+        byte liczba1 = 120;
+        short liczba2 = 300;
+        int liczba3 = 12321321;
+        long liczba4 = 123213232;
+        System.out.println("liczba całkowita: "+liczba2);
+
+        //typy zmiennoprzecinkowe
+        float liczba5 = 12.546f;
+        double liczba6 = 213123.321321;
+
+        //typ logiczny
+        boolean czyToTo = true;
+
+        //typ znakowy
+        char znak = 'a';
+
+
+
+
     }
 }
